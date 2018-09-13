5a7e30eb29832cb97bcebcb4eb56fe0c0e1f3e23
PM <pmsahnoune@lenormant.fr>
2018-09-13 12:13:49 +0200

    Actoin dans controller

--- Controller/mainController.php
+++ Controller/mainController.php
@@ -1,4 +1,45 @@
 <?php
-$sn = explode("\n",$_POST["SN"]);
-print_r ($sn);
+
+include('..\Connexion.php');
+if ($_GET['action'] == 'ajoutCommande') {
+  $controller=new controller($bdd);
+  $controller->ajoutCommandeBDD();
+  }
+
+  class controller
+{
+  private $_db;
+  function __construct($db)
+  {
+    $this->setDb($db);
+  }
+  public function ajoutCommandeBDD(){
+    $sn = explode("\n",$_POST['SN']);
+    $nombreDeSN = count($sn) -1;
+    $etat =0;
+    $requete = $this->_db->prepare('INSERT INTO gestion_commande (SN, Commande, Date_commande, Date_reception, Fournisseur_ID, etat)
+                          VALUES (:sn, :commande, :orderDate, :deliveryDate, :supplier, :etat)');
+
+
+    for($x = 0; $x < $nombreDeSN; $x++) {
+
+    $requete->execute(array(
+      'sn'=> $sn[$x],
+      'commande' => $_POST['commande'],
+      'orderDate' => $_POST['orderDate'],
+      'deliveryDate' => $_POST['deliveryDate'],
+      'supplier' => $_POST['supplier'],
+      'etat' => $etat
+    ));
+    echo $sn[$x];
+    echo "<br>";
+}
+  }
+  public function setDb(PDO $db){
+    $this->_db=$db;
+  }
+}
+
+
+
 ?>
--- Templates/Add.php
+++ Templates/Add.php
@@ -6,19 +6,19 @@
   </head>
   <body>
     <h1>Ajout d'une commande</h1>
-    <form action="../Controller/mainController.php" method="POST">
+    <form action="../Controller/mainController.php?action=ajoutCommande" method="POST">
       Numéro de commande:<br>
       <input type="text" name="commande" value="Numero de commande"><br>
       Date de commande:<br>
-      <input type="date" name="orderDate" value="JJ/MM/AAAA"><br>
+      <input type="date" name="orderDate" value="AAAA-MM-JJ"><br>
       Date de réception:<br>
-      <input type="date" name="deliveryDate" value="JJ/MM/AAAA"><br>
+      <input type="date" name="deliveryDate" value="AAAA-MM-JJ"><br>
       Fournisseur :<br>
       <select name="supplier">
-        <option value="LDLC.com">LDLC</option>
-        <option value="Orange">Orange</option>
-        <option value="DEFI60">DEFI60</option>
-        <option value="TECHDATA">TECHDATA</option>
+        <option value="1">LDLC</option>
+        <option value="2">Orange</option>
+        <option value="3">DEFI60</option>
+        <option value="4">TECHDATA</option>
       </select><br>
       Numéros de série des matériels :<br>
       <textarea name="SN" rows="20" cols="100"></textarea>
